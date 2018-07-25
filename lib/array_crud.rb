28b2d40a0097335bde6d18208a54f70a880d1f63
Sharmaine Requerme <srqrme@gmail.com>
2018-07-25 15:39:35 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  santas_reindeer = ["Dasher", "Dancer", "Prancer", "Comet"]
+  ["1", "2", "3", "4"]
 end
 
 def add_element_to_end_of_array(array, element)
@@ -22,14 +22,14 @@ def remove_element_from_start_of_array(array)
   array.shift
 end
 
-def retrieve_element_from_index(array, index_number)
-  array[index_number]
+def retrieve_element_from_index(array, index)
+  array[index]
 end
 
 def retrieve_first_element_from_array(array)
-  array.first
+  array[0]
 end
 
 def retrieve_last_element_from_array(array)
-  array.last
+  array[-1]
 end
